17eac8c0deee3a5831e90546b3d4dd4f7c11acda
HoangTrieuVy-LE <34876925+HoangTrieuVy@users.noreply.github.com>
2022-10-18 16:23:17 +0200

    Update tools_dms.py

--- python_dms/lib/tools_dms.py
+++ python_dms/lib/tools_dms.py
@@ -4,33 +4,33 @@ from matplotlib import pyplot as plt
 from scipy.sparse import diags
 import time
 from dms import *
-from skimage.metrics import structural_similarity as ssim
-from skimage.metrics import mean_squared_error
-from skimage.metrics import peak_signal_noise_ratio
-import torch
-from torch import nn
-def cross_entropy_loss2d(inputs, targets, cuda=False, balance=1.1):
-    """
-    :param inputs: inputs is a 4 dimensional data nx1xhxw
-    :param targets: targets is a 3 dimensional data nx1xhxw
-    :return:
-    """
-    n, c, h, w = inputs.size()
-    weights = np.zeros((n, c, h, w))
-    for i in range(n):
-        t = targets[i, :, :, :].cpu().data.numpy()
-        pos = (t == 1).sum()
-        neg = (t == 0).sum()
-        valid = neg + pos
-        weights[i, t == 1] = neg * 1. / valid
-        weights[i, t == 0] = pos * balance / valid
-    weights = torch.Tensor(weights)
-    if cuda:
-        weights = weights.cuda()
-#     print(weights)
-    inputs = torch.sigmoid(inputs)
-    loss=nn.BCELoss(weights,reduction='mean')(inputs,targets)     #loss=nn.BCELoss(weights,size_average=False)(inputs,targets)
-    return loss
+#from skimage.metrics import structural_similarity as ssim
+#from skimage.metrics import mean_squared_error
+#from skimage.metrics import peak_signal_noise_ratio
+#import torch
+#from torch import nn
+# def cross_entropy_loss2d(inputs, targets, cuda=False, balance=1.1):
+#     """
+#     :param inputs: inputs is a 4 dimensional data nx1xhxw
+#     :param targets: targets is a 3 dimensional data nx1xhxw
+#     :return:
+#     """
+#     n, c, h, w = inputs.size()
+#     weights = np.zeros((n, c, h, w))
+#     for i in range(n):
+#         t = targets[i, :, :, :].cpu().data.numpy()
+#         pos = (t == 1).sum()
+#         neg = (t == 0).sum()
+#         valid = neg + pos
+#         weights[i, t == 1] = neg * 1. / valid
+#         weights[i, t == 0] = pos * balance / valid
+#     weights = torch.Tensor(weights)
+#     if cuda:
+#         weights = weights.cuda()
+# #     print(weights)
+#     inputs = torch.sigmoid(inputs)
+#     loss=nn.BCELoss(weights,reduction='mean')(inputs,targets)     #loss=nn.BCELoss(weights,size_average=False)(inputs,targets)
+#     return loss
 
 def save_results(a,b,c,link):
     
